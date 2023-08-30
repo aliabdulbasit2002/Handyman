56c4cba8d2065a869f9d28d22a144dc115023f7c
dekin <dekinlinux@gmail.com>
2023-08-30 21:51:38 +0000

    worked in the category

--- src/Pages/ActiveBooking.jsx
+++ src/Pages/ActiveBooking.jsx
@@ -6,14 +6,19 @@ import { MdArrowBackIos } from "react-icons/md";
 import axios from "axios";
 
 function ActiveBooking() {
+  // GET USER ID FROM LOCALSTORAGE
+  let userId = localStorage.getItem("user");
+  let userData = JSON.parse(userId);
+
   const navigate = useNavigate();
-
   const [requests, setRequests] = useState([]);
-
   useEffect(() => {
+    // let userId = localStorage.getItem("user");
+    // let userData = JSON.parse(userId);
     const requestData = async () => {
-      const { data } = await axios.get("http://localhost:3001/request");
-
+      const data = await axios.get(
+        "http://localhost:3001/request/requestById",
+      );
       setRequests(data);
     };
     requestData();
@@ -27,24 +32,28 @@ function ActiveBooking() {
         </span>
         Active Booking
       </Heading>
+      {}
 
       {/* LIST OF ACTIVE BOOKINGS */}
-      {requests.map((request) => {
-        const { business, client, description, address, updatedAt } = request;
-        return (
-          <Active
-            businessName={business?.businessName}
-            name={client?.fullname}
-            category={business?.category}
-            rating={business?.rating}
-            employees={business?.workers}
-            date={updatedAt}
-            location={address}
-            description={description}
-            isVerified={business?.isVerified}
-          />
-        );
-      })}
+      {requests &&
+        requests.map((request, index) => {
+          const { business, client, description, address, requestDate } =
+            request;
+          return (
+            <Active
+              key={index}
+              businessName={business?.businessName}
+              name={client?.fullname}
+              category={business?.category}
+              rating={business?.ratings}
+              employees={business?.workers}
+              date={new Date(requestDate).toDateString()}
+              location={address}
+              description={description}
+              isVerified={business?.isVerified}
+            />
+          );
+        })}
     </Box>
   );
 }
